3a64de6ca41fdb91000dd0daf0b8e5112f6470e2
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2021-06-09 01:11:30 +0300

    Update confetti.js

--- confetti.js
+++ confetti.js
@@ -47,10 +47,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var speed = 50,
       duration = (1.0 / speed),
       confettiRibbonCount = 11,
-      ribbonPaperCount = 65,
+      ribbonPaperCount = 30,
       ribbonPaperDist = 8.0,
       ribbonPaperThick = 8.0,
-      confettiPaperCount = 200,
+      confettiPaperCount = 100,
       DEG_TO_RAD = PI / 180,
       RAD_TO_DEG = 180 / PI,
       colors = [
